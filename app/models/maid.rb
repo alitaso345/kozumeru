73458ee1c49288d439b77c06aee8b62f9e137435
alice345 <alice.maru345@gmail.com>
2015-03-30 02:38:57 +0900

    スクレイピングしてきたデータをDBにimportするメソッドを作成

--- app/models/maid.rb
+++ app/models/maid.rb
@@ -2,4 +2,18 @@ class Maid < ActiveRecord::Base
   validates :name, presence: true
 
   has_one :twitter_account
+
+  def self.import_info_from_homepage
+    lists = HomePageCrawler.get_maid_lists
+    client = TwitterClient.new
+    lists.each do |list|
+      begin
+        maid = Maid.create(name: list[:name], floor: list[:floor], maidnumber: list[:maid_number])
+        TwitterAccount.create(uid: client.get_user_uid(list[:screen_name]), screen_name: list[:screen_name], maid_id: maid.id)
+      rescue => e
+        p e
+        p "#{list[:name]} failed import data"
+      end
+    end
+  end
 end
