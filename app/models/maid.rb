53d366c12b002f514464f35c43a07d1e75d7dde7
alice345 <alice.maru345@gmail.com>
2015-04-05 13:02:17 +0900

    API制限を食らうのを防ぐため，TwitterAccount作成の際は間をあける

--- app/models/maid.rb
+++ app/models/maid.rb
@@ -10,6 +10,7 @@ class Maid < ActiveRecord::Base
       begin
         maid = Maid.create(name: list[:name], floor: list[:floor], maidnumber: list[:maid_number])
         TwitterAccount.create(uid: client.get_user_uid(list[:screen_name]), screen_name: list[:screen_name], maid_id: maid.id)
+        sleep 0.5 #連続してアクセスしすぎるとAPI制限を食らうため
       rescue => e
         p e
         p "#{list[:name]} failed import data"
