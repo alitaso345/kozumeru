d0700e1eeea1a97f236bb6f499ac7934d00936f8
alice345 <alice.maru345@gmail.com>
2015-04-12 11:49:19 +0900

    画像一覧表示のためのresourcesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root :to => 'maids#index'
   resources :maids, :only => ['index', 'show', 'edit'] do
     resources :tweets, :only => ['index']
+    resources :pictures, :only => ['index']
   end
 end
