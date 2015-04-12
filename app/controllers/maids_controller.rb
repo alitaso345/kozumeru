88738e43ec31fd041fc5da1b5074cecee39d201f
alice345 <alice.maru345@gmail.com>
2015-04-12 17:52:30 +0900

    メイドさんの詳細ページを作成

--- app/controllers/maids_controller.rb
+++ app/controllers/maids_controller.rb
@@ -2,4 +2,10 @@ class MaidsController < ApplicationController
   def index
     @maids = Maid.all
   end
+
+  def show
+    @maid = Maid.find(params[:id])
+    @tweets = @maid.tweets.limit(50)
+    @pictures = @maid.pictures.take(10)
+  end
 end
