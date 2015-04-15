78e5dea8daaf1706ad56aaa68e76766323a279ea
alice345 <alice.maru345@gmail.com>
2015-04-15 22:37:24 +0900

    tweetとpictureは1対nの関係にあるので関連の宣言を修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,7 @@ class Tweet < ActiveRecord::Base
   validates :published_at, presence: true
 
   belongs_to :twitter_account
-  has_one :picture
+  has_many :picture
 
   def self.import_recent_tweet
     accounts = TwitterAccount.all.select('id, screen_name')
