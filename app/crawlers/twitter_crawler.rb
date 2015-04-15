fcece6efd90f77476fd7bdfc24f9e34b8354dd13
alice345 <alice.maru345@gmail.com>
2015-04-15 22:39:11 +0900

    Twitterのクローラーを行うためのクラスを作成
    Tweetクラスにあったメソッドを移植

---
+++ app/crawlers/twitter_crawler.rb
@@ -0,0 +1,23 @@
+class TwitterCrawler
+  def self.import_recent_tweet
+    accounts = TwitterAccount.all.select('id, screen_name').limit(1)
+    client = TwitterClient.new
+
+    accounts.each do |account|
+      begin
+        client.get_recent_tweets(account).each do |tweet|
+          created_tweet = Tweet.create({twitter_account_id: account.id, text: tweet.text, status_id:  tweet.id, published_at: tweet.created_at})
+
+          if tweet.media?
+            tweet.media.select{|media| media.class == Twitter::Media::Photo}.each do |photo|
+              Picture.create({tweet_id: created_tweet.id, url: photo.media_url.to_s})
+            end
+          end
+        end
+      rescue => e
+        p e
+        p "failed import #{account.screen_name}'s tweet"
+      end
+    end
+  end
+end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,28 +7,4 @@ class Tweet < ActiveRecord::Base
   belongs_to :twitter_account
   has_many :picture
 
-  def self.import_recent_tweet
-    accounts = TwitterAccount.all.select('id, screen_name')
-    client = TwitterClient.new
-
-    accounts.each do |account|
-      begin
-        client.get_recent_tweets(account).each do |tweet|
-          tw = Tweet.create({twitter_account_id: account.id, text: tweet.text, status_id:  tweet.id, published_at: tweet.created_at})
-
-          if has_media?(tweet)
-            Picture.create({tweet_id: tw.id, url: tweet.media.first.media_url})
-          end
-        end
-      rescue => e
-        p e
-        p "failed import #{account.screen_name}'s tweet"
-      end
-    end
-  end
-
-  private
-  def self.has_media?(tweet)
-    (tweet.media? && tweet.media.first.media_url) ? true : false
-  end
 end
