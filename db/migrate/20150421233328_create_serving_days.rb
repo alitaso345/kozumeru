8fc2800ba443e0357594449f10033b7e3381302e
alice345 <alice.maru345@gmail.com>
2015-04-22 09:01:19 +0900

    migrationファイルに必要なカラムを追記

--- db/migrate/20150421233328_create_serving_days.rb
+++ db/migrate/20150421233328_create_serving_days.rb
@@ -1,6 +1,13 @@
 class CreateServingDays < ActiveRecord::Migration
   def change
     create_table :serving_days do |t|
+      t.references :maid, null: false, index: true
+      t.references :picture, null: false, index: true
+      t.date :date
+      t.time :time
+      t.integer :location, default: 0, null: false
+      t.string :miss_analyzed
+      t.timestamps null: false
     end
   end
 end
