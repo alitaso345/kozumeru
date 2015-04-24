06f60b5b418ad4ed234dee9505490f37e6239398
gaoch <misako3210@hotmail.com>
2015-04-25 05:29:26 +0900

    face_judgingメソッド追加、picture_analyzingメソッドの修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -6,9 +6,18 @@ class Picture < ActiveRecord::Base
 
   enum kind: {serve: 0, maid: 1, other: 3}
 
-  def picture_analizing
-    pic =  Picture.where(:id => id).first
-    pic.update_attribute(:analized, true)
+  def picture_analyzing
+    pic = Picture.find_by(:id => id)
+    pic.update(:analyzed => true)
   end
+
+  def face_judging(id)
+    api = DocomoAPI.new
+    pic = Picture.where(:id => id).first
+    #if api.face_judgiment(pic.url)
+    pic.update_attribute(:kind, 'maid')
+    #end
+  end
+
 
 end
