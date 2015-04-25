4aceb62f3ae01b54eebbc94f54af8dd17156022f
alice345 <alice.maru345@gmail.com>
2015-04-25 10:40:04 +0900

    postsのテーブル情報を記述

--- db/migrate/20150425012959_create_posts.rb
+++ db/migrate/20150425012959_create_posts.rb
@@ -1,6 +1,14 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.references :blog, null: false, index: true
+      t.string :url, null: false
+      t.string :title, null: false
+      t.text :body, null: false
+      t.datetime :published_at, null: false
+      t.timestamps null: false
     end
+
+    add_index :posts, :url, :unique => true
   end
 end
