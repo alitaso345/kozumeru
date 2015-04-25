c6d99be8eb52b7a6fdb9e130bff683c18218f967
alice345 <alice.maru345@gmail.com>
2015-04-25 15:09:14 +0900

    例外処理があるとエラーが起きた時に見通しが悪くなるので一旦削除

--- app/crawlers/home_page_crawler.rb
+++ app/crawlers/home_page_crawler.rb
@@ -12,43 +12,46 @@ class HomePageCrawler
 
   def self.get_maid_lists
     maids_list = []
-    begin
-      #クロールの起点となるURLを指定
-      urls = [
-        'http://www.cafe-athome.com/maids/free/',
-        'http://www.cafe-athome.com/maids/honten7f/',
-        'http://www.cafe-athome.com/maids/honte16f/',
-        'http://www.cafe-athome.com/maids/honten4f/',
-        'http://www.cafe-athome.com/maids/donki/',
-        'http://www.cafe-athome.com/maids/new/'
-      ]
+    #クロールの起点となるURLを指定
+    urls = [
+      'http://www.cafe-athome.com/maids/free/',
+      'http://www.cafe-athome.com/maids/honten7f/',
+      'http://www.cafe-athome.com/maids/honte16f/',
+      'http://www.cafe-athome.com/maids/honten4f/',
+      'http://www.cafe-athome.com/maids/donki/',
+      'http://www.cafe-athome.com/maids/new/'
+    ]
 
-      Anemone.crawl(urls, :depth_limit  => 1, :skip_query_strings => true) do |anemone|
-        #巡回先の絞り込み
-        anemone.focus_crawl do |page|
-          page.links.keep_if{ |link|
-            link.to_s.match(/http:\/\/www.cafe-athome.com\/maids/)
-          }
+    Anemone.crawl(urls, :depth_limit  => 1, :skip_query_strings => true) do |anemone|
+      #巡回先の絞り込み
+      anemone.focus_crawl do |page|
+        page.links.keep_if{ |link|
+          link.to_s.match(/http:\/\/www.cafe-athome.com\/maids/)
+        }
+      end
+
+      #取得したページに対する処理
+      anemone.on_pages_like(/maids\/\d{1,}/) do |page|
+        doc = Nokogiri::HTML.parse(page.body)
+
+        info = Hash.new
+        info[:number] = page.url.to_s.match(/\d{2,}/).to_s
+        info[:name] = doc.xpath("//div[@id='maid-name']").text
+        doc.xpath("//*[@id='maid-properties']/dl[3]/dd/a").each do |node|
+          info[:screen_name] = node.attributes["href"].text.to_s.gsub(/https:\/\/twitter.com\//, "")
         end
+        info[:floor] = doc.xpath("//*[@id='maid-properties']/dl[1]/dd/a[1]").text
+        info[:blog_url] = get_blog_url(doc)
+        maids_list << info
+      end
+    end 
 
-        #取得したページに対する処理
-        anemone.on_pages_like(/maids\/\d{1,}/) do |page|
-          doc = Nokogiri::HTML.parse(page.body)
+    maids_list
+  end
 
-          info = Hash.new
-          info[:number] = page.url.to_s.match(/\d{2,}/).to_s
-          info[:name] = doc.xpath("//div[@id='maid-name']").text
-          doc.xpath("//*[@id='maid-properties']/dl[3]/dd/a").each do |node|
-            info[:screen_name] = node.attributes["href"].text.to_s.gsub(/https:\/\/twitter.com\//, "")
-          end
-          info[:floor] = doc.xpath("//*[@id='maid-properties']/dl[1]/dd/a[1]").text
-          info[:blog_url] = doc.xpath("//*[@id='maid-properties']/dl[2]/dd/a").first.attributes["href"].value
-          maids_list << info
-        end
-      end 
-      maids_list
-    rescue => e
-      p e
-    end
+  private
+  def self.get_blog_url(doc)
+    link = doc.xpath("//*[@id='maid-properties']/dl[2]/dd/a").first
+    link ? link.attributes["href"].value : nil
   end
 end
