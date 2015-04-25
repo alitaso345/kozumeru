6478c771a0b0710c3e6699b3354ede3fde635a8d
alice345 <alice.maru345@gmail.com>
2015-04-25 10:26:15 +0900

    blogsに対するmigrationを実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418124944) do
+ActiveRecord::Schema.define(version: 20150425010327) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.integer  "maid_id",    null: false
+    t.string   "title",      null: false
+    t.string   "url",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "blogs", ["maid_id"], name: "index_blogs_on_maid_id"
+  add_index "blogs", ["url"], name: "index_blogs_on_url", unique: true
 
   create_table "maids", force: :cascade do |t|
     t.string   "name",                       null: false
