aa9a00380a43312e9a85a12a87d420cda794a5ee
alice345 <alice.maru345@gmail.com>
2015-04-25 10:12:12 +0900

    blogsのカラムを記述

--- db/migrate/20150425010327_create_blogs.rb
+++ db/migrate/20150425010327_create_blogs.rb
@@ -1,6 +1,12 @@
 class CreateBlogs < ActiveRecord::Migration
   def change
     create_table :blogs do |t|
+      t.references :maid, null: false, index: true
+      t.string :title, null: false
+      t.string :url, null: false
+      t.timestamps null: false
     end
+
+    add_index :blogs, :url, :unique => true
   end
 end
