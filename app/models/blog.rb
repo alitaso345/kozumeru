19711c4ef773f64d13cbe1b5fdf74537d9ead17a
alice345 <alice.maru345@gmail.com>
2015-04-25 15:09:55 +0900

    fix typo

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ActiveRecord::Base
   validates :title, presence: true
-  validates :url, presence: true, uniqueness: true
+  validates :account_name, presence: true, uniqueness: true
 
   belongs_to :maid
   has_many :posts
