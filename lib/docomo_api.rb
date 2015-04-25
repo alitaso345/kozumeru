e76e619a14f11cdec3e04bd6b2737143bcbf818a
gaoch <misako3210@hotmail.com>
2015-04-25 14:27:37 +0900

    meru/docomo_api.rbの画像認識に関わる部分を移植

--- lib/docomo_api.rb
+++ lib/docomo_api.rb
@@ -1,14 +1,58 @@
 class DocomoAPI
   def initialize
-    api = Rails.application.secrets[:docomo_api_key] 
-    @uri = URI('https://api.apigw.smt.docomo.ne.jp/puxImageRecognition/v1/faceDetection')
-    @uri.query = 'APIKEY=' + api
+    @id = 0
+    @api_key = Rails.application.secrets[:docomo_api_key] 
+    @uri = URI('https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/document')
   end
 
-#渡された画像URLに顔が写っているかどうかを判定
-  def face_judgement(picture_url)
+  ###   画像認識要求   ###
+  def req_ocr(picture)
+    @uri.query = 'APIKEY=' + @api_key
+
     response = RestClient.post(
       @uri.to_s,
+      {:image => File.open(picture),
+       :lang => 'jpn'
+    },
+    :content_type => 'multipart/form-data'
+    )
+
+    # 得られた結果からidのみ出力
+    hash = JSON.parser.new(response).parse()
+    @id = hash['job']['@id']
+  end
+
+
+  ###   画像認識結果取得   ###
+  def get_ocr
+    @uri.path += "/" +  @id
+    loop do
+      result = RestClient.get(@uri.to_s)
+      @hash = JSON.parser.new(result).parse()
+      # 処理に成功した場合のみ処理を続行
+      if @hash['job']['@status']=="success"
+        break
+      elsif @hash['job']['@status']!="process"
+        p "失敗もしくは削除済みです"
+        exit
+      end
+      sleep(0.5)#連続してRestClientすると怒られるのでお休み
+    end
+
+    # 得られたjsonのうち@textのみ出力
+    text =  @hash['lines']['line']   
+    text.each do |text|
+      # pattern(text['@text']) rescue nil
+      p jpndate(pattern(text['@text'])) rescue nil
+    end
+  end
+
+  ###  渡された画像URLに顔が写っているかどうかを判定 ###
+  def face_judgement(picture_url)
+    uri = URI('https://api.apigw.smt.docomo.ne.jp/puxImageRecognition/v1/faceDetection')
+    uri.query = 'APIKEY=' + @api_key
+    response = RestClient.post(
+      uri.to_s,
       {
         :imageURL => picture_url,
         :response => 'json'
@@ -18,4 +62,40 @@ class DocomoAPI
     hash = JSON.parser.new(response).parse()
     true if hash["results"]["faceRecognition"]["detectionFaceNumber"]
   end
+
+
+
+  private
+  # 文章をgrepで整形
+  def pattern(str)
+    str.gsub!(/[旧碑〇丨|I）}]/,
+              "旧"=>"1日",
+              "碑"=>"4年",
+              "〇"=>"0",
+              /[丨|I]/=>"1",
+              /[）}>]/=>")")
+    str.gsub(" ","")
+  end
+
+  #日付の文字列(XXXX年YY月ZZ日)からdateオブジェクトを得る
+  def jpndate(str)
+    @year=@month=@day=0
+    str.scan(/(\d+)(年|月|日)/) do
+      case $2 
+      when "年"
+        @year = $1.to_i
+      when "月"
+        @month = $1.to_i
+      when "日"
+        @day = $1.to_i
+      end
+    end
+    # 得られた日付が存在する場合のみdateオブジェクトを返す
+    if Date.valid_date?(@year, @month, @day)
+      return Date.new(@year, @month, @day).to_s
+    else
+      return str
+    end
+  end
 end
+
