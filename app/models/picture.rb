be5d303936f361a95da7e47cd00f0aa2365dd0a0
alice345 <alice.maru345@gmail.com>
2015-04-26 22:46:17 +0900

    validatesを修正

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,9 +1,8 @@
 class Picture < ActiveRecord::Base
-  validates :tweet_id, presence: true
   validates :url, presence: true, uniqueness: true
 
   belongs_to :tweet
   belongs_to :post
 
-  enum kind: {serve: 0, maid: 1, other: 3}
+  enum kind: {serve: 0, maid: 1, other: 2}
 end
