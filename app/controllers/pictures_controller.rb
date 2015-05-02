06698761a8104fd0ac2daab889b68cdbd4f51e77
alice345 <alice.maru345@gmail.com>
2015-05-03 01:08:10 +0900

    kindがnilのときはすべての画像を表示するように修正
    ただこのif文キモいのでなんとかしたい.とりえあえず動くのでコミット

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,9 +2,17 @@ class PicturesController < ApplicationController
   def index
     if params[:maid_id]
       maid = Maid.find(params[:maid_id])
-      @pictures = maid.pictures.page(params[:page]).per(10)
+      if params[:kind]
+        @pictures = maid.pictures.where(kind: params[:kind]).page(params[:page]).per(10)
+      else
+        @pictures = maid.pictures.page(params[:page]).per(10)
+      end
     else
-      @pictures = Picture.where(kind: params[:kind]).page(params[:page]).per(10)
+      if params[:kind]
+        @pictures = Picture.where(kind: params[:kind]).page(params[:page]).per(10)
+      else
+        @pictures = Picture.all.page(params[:page]).per(10)
+      end
     end
   end
 end
