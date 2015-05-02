4a2f3c5af321f5f0a941ce8f74fd74225c568f05
alice345 <alice.maru345@gmail.com>
2015-05-03 00:49:24 +0900

    メイドさんの個別のページでもページネーションを使えるようにする

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   def index
     if params[:maid_id]
       maid = Maid.find(params[:maid_id])
-      @pictures = maid.pictures
+      @pictures = maid.pictures.page(params[:page]).per(10)
     else
       @pictures = Picture.where(kind: params[:kind]).page(params[:page]).per(10)
     end
