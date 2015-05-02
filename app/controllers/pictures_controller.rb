a659000f98f99f93775c370ad1734e49673e730a
alice345 <alice.maru345@gmail.com>
2015-05-03 01:24:57 +0900

    画像の種類を振り分けるためのメソッドを追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,17 +2,23 @@ class PicturesController < ApplicationController
   def index
     if params[:maid_id]
       maid = Maid.find(params[:maid_id])
-      if params[:kind]
-        @pictures = maid.pictures.where(kind: params[:kind]).page(params[:page]).per(10)
-      else
-        @pictures = maid.pictures.page(params[:page]).per(10)
-      end
+      @pictures = maid.pictures.where(kind: kind_params).page(params[:page]).per(10)
     else
-      if params[:kind]
-        @pictures = Picture.where(kind: params[:kind]).page(params[:page]).per(10)
+      @pictures = Picture.where(kind: kind_params).page(params[:page]).per(10)
+    end
+  end
+
+  private
+  def kind_params
+    case params[:kind]
+      when "serve"
+        0
+      when "maid"
+        1
+      when "other"
+        2
       else
-        @pictures = Picture.all.page(params[:page]).per(10)
-      end
+        [nil, 0, 1, 2]
     end
   end
 end
