b0d1544eae5e122fcb6a19066cbdb71472c331e8
alice345 <alice.maru345@gmail.com>
2015-05-03 02:34:59 +0900

    数字を直接書くと，今後修正するときに大変そうなのでenumのメソッドを使う

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,7 @@ class PicturesController < ApplicationController
   def index
     if params[:maid_id]
       maid = Maid.find(params[:maid_id])
-      @pictures = maid.pictures.where(kind: kind_params).page(params[:page]).per(10)
+      @pictures = maid.pictures().where(kind: kind_params).page(params[:page]).per(10)
     else
       @pictures = Picture.where(kind: kind_params).page(params[:page]).per(10)
     end
@@ -10,15 +10,10 @@ class PicturesController < ApplicationController
 
   private
   def kind_params
-    case params[:kind]
-      when "serve"
-        0
-      when "maid"
-        1
-      when "other"
-        2
-      else
-        [nil, 0, 1, 2]
+    if params[:kind]
+      Picture.kinds[params[:kind].to_sym]
+    else
+      [nil] + Picture.kinds.values
     end
   end
 end
