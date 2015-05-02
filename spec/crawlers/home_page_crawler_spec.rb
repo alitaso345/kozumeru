abd3016e8d6a64eaef276b68222c17c5333ab3cf
alice345 <alice.maru345@gmail.com>
2015-05-02 12:19:39 +0900

    年は固定ではなく，その年に合わせる

--- spec/crawlers/home_page_crawler_spec.rb
+++ spec/crawlers/home_page_crawler_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 describe HomePageCrawler do
   describe '#get_start_time' do
-    context '2015年の"5/1【本店6階】15:00～22:00の"のとき' do
-      it '2015-05-01 15:00:00 であること' do
+    context '"5/1【本店6階】15:00～22:00の"のとき' do
+      it '05-01 15:00:00 であること' do
         crawler = HomePageCrawler.new
         text = "5/1【本店6階】15:00～22:00の"
 
-        expect(crawler.get_start_time(text)).to eq(Time.new(2015, 5, 1, 15, 00))
+        expect(crawler.get_start_time(text)).to eq(Time.new(DateTime.now.year, 5, 1, 15, 00))
       end
     end
   end
 
   describe '#get_end_time' do
-    context '2015年の"5/1【本店6階】15:00～22:00の"のとき' do
-      it '2015-05-01 22:00:00 であること' do
+    context '"5/1【本店6階】15:00～22:00の"のとき' do
+      it '05-01 22:00:00 であること' do
         crawler = HomePageCrawler.new
         text = "5/1【本店6階】15:00～22:00の"
-        expect(crawler.get_end_time(text)).to eq(Time.new(2015, 5, 1, 22, 00))
+        expect(crawler.get_end_time(text)).to eq(Time.new(DateTime.now.year, 5, 1, 22, 00))
       end
     end
   end
 
   describe '#get_location' do
-    context '2015年の"5/1【本店6階】15:00～22:00の"のとき' do
+    context '"5/1【本店6階】15:00～22:00の"のとき' do
       it 'sixth_floor であること' do
         crawler = HomePageCrawler.new
         text = "5/1【本店6階】15:00～22:00の"
