7471599678ad779a4608526a60aa275f3db0acf4
alice345 <alice.maru345@gmail.com>
2015-05-02 13:06:20 +0900

    時刻取得の部分をリファクタリングするため，メソッドを1つにまとめる

--- spec/crawlers/home_page_crawler_spec.rb
+++ spec/crawlers/home_page_crawler_spec.rb
@@ -1,23 +1,19 @@
 require 'rails_helper'
 
 describe HomePageCrawler do
-  describe '#get_start_time' do
+  describe '#service_time'do
     context '"5/1【本店6階】15:00～22:00の"のとき' do
-      it '05-01 15:00:00 であること' do
-        crawler = HomePageCrawler.new
-        text = "5/1【本店6階】15:00～22:00の"
+      crawler = HomePageCrawler.new
+      text = "5/1【本店6階】15:00～22:00の"
 
-        expect(crawler.get_start_time(text)).to eq(Time.new(DateTime.now.year, 5, 1, 15, 00))
+      service_time = crawler.service_time(text)
+
+      it '開始時間が05-01 15:00:00 であること' do
+        expect(service_time[:start]).to eq(Time.new(DateTime.now.year, 5, 1, 15, 00))
       end
-    end
-  end
 
-  describe '#get_end_time' do
-    context '"5/1【本店6階】15:00～22:00の"のとき' do
-      it '05-01 22:00:00 であること' do
-        crawler = HomePageCrawler.new
-        text = "5/1【本店6階】15:00～22:00の"
-        expect(crawler.get_end_time(text)).to eq(Time.new(DateTime.now.year, 5, 1, 22, 00))
+      it '終了時間が05-01 22:00:00 であること' do
+        expect(service_time[:end]).to eq(Time.new(DateTime.now.year, 5, 1, 22, 00))
       end
     end
   end
